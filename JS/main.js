7ab9de5323f328e5f0368ac3748b6e0f76df8dee
Cassetete <timotheevdbosch@gmail.com>
2025-01-27 21:01:20 +0100

    fix: gsap import 2

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,6 @@
-import { gsap } from "./node_modules/gsap/index.js";
-import { ScrollTrigger } from "./node_modules/gsap/ScrollTrigger.js";
+import { gsap } from "../node_modules/gsap/index.js";
+import { ScrollTrigger } from "../node_modules/gsap/ScrollTrigger.js";
+
 import { my_projects, my_projects_actu, coding, design, CMS } from './data_projects.min.js';
 
 gsap.registerPlugin(ScrollTrigger);
